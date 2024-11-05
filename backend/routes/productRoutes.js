9d27124946526116131c4b0b3dc99b80a23acd03
Sercan <sercansimsek44@gmail.com>
2024-11-05 17:23:42 +0100

    data fetched from api

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,30 @@
+import express from "express";
+import asyncHandler from "../middleware/asyncHandler.js";
+import Product from "../models/productModel.js";
+
+const router = express.Router();
+
+// route for all products
+router.get(
+	"/",
+	asyncHandler(async (req, res) => {
+		const products = await Product.find({});
+		res.json(products);
+	})
+);
+
+// route for individual product
+router.get(
+	"/:id",
+	asyncHandler(async (req, res) => {
+		const product = await Product.findById(req.params.id);
+
+		if (product) {
+			return res.json(product);
+		}
+
+		res.status(404).json({ message: "Product not found" });
+	})
+);
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 import { connectDB } from "./config/db.js";
-import products from "./data/products.js";
+// import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const port = process.env.PORT || 8000;
 connectDB(); // Connect to MongoDB
@@ -13,15 +14,18 @@ app.get("/", (req, res) => {
 	res.send("API is running...");
 });
 
-// route for all products
+app.use("/api/products", productRoutes);
+
+/* 
+route for all products
 app.get("/api/products", (req, res) => {
 	res.json(products);
 });
 
-// route for individual product
+route for individual product
 app.get("/api/products/:id", (req, res) => {
 	const product = products.find((p) => p._id === req.params.id);
 	res.json(product);
-});
+}); */
 
 app.listen(port, () => console.log(`server running on port ${port}`));
